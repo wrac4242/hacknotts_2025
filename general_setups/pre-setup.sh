f6659352ba61f99228c8680e01e634c4dcb4f6e6
Wren L. Alexander <wren.alexander@student.manchester.ac.uk>
2025-10-25 19:48:44 +0100

    add more setup script stuff

--- general_setups/pre-setup.sh
+++ general_setups/pre-setup.sh
@@ -2,8 +2,12 @@
 
 rm /home/background_scripts/background_scripts.sh
 touch /home/background_scripts/background_scripts.sh
-chmod +x /home/background_scripts/background_scripts.sh
+touch /home/background_scripts/prerunning.sh
+chmod +x /home/background_scripts/{background_scripts,prerunning}.sh
 mkdir /home/background_scripts/scripts
 mkdir -p /home/background_scripts/logs/{out,err}
 
 chown -R background_scripts:background_scripts /home/background_scripts
+
+# installing
+apt install python3-pip
--- wren-charlie/setup.sh
+++ wren-charlie/setup.sh
@@ -2,3 +2,4 @@ echo "python3 ~/scripts/wren_charlie.py > ~/logs/out/wren_charlie.txt 2> ~/logs/
 cp listening_script.py /home/background_scripts/scripts/wren_charlie.py
 chown background_scripts:background_scripts /home/background_scripts/scripts/wren_charlie.py
 
+echo "pip install inotify-simple" >> /home/background_scripts/prerunning.sh
