4ff37a786a2448f9bf4fab40b12b0920fe2fdf94
David Jeffreys <179866819+moorepastas@users.noreply.github.com>
2025-10-25 19:55:18 +0100

    part2

---
+++ general_setups/setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+file_loc="$(dirname "$0")/.."
+cd "$file_loc" || exit
+
+base=$(pwd)
+
+
+# find all the setup files
+file_list=$(find -H "$base" -name setup.sh -not -path "*/general_setups/setup.sh" )
+readarray -t files <<<"$file_list"
+
+for file in "${files[@]}"
+do
+    dir=$(dirname "$file")
+    cd "$dir" || return
+    ./setup.sh
+    
+done
