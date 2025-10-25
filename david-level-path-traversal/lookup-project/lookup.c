99383c72e99f286a92d404da1872f512cdb3eb84
David Jeffreys <179866819+moorepastas@users.noreply.github.com>
2025-10-25 14:35:41 +0100

    add c file

--- david-level-path-traversal/lookup-project/lookup.c
+++ david-level-path-traversal/lookup-project/lookup.c
@@ -1,2 +1,28 @@
 // notes for me
 // get a string from the user then find the appropiate file and read it (make sure it is traversable)
+
+#include <stdio.h>
+
+int main() {
+
+    // find the user's flower choice
+    char query[2048]; // TODO: do we need such a big buffer?
+    print("Enter flower name for lookup: ");
+    scanf("%s", &query);
+
+    //read the file
+
+    char filename[2048];
+    sprintf(filename, "flowers/%s");
+
+    char file_content[2048];
+
+    FILE *flower_file = fopen(filename,"r");
+    fgets(file_content, 2048, flower_file);
+
+    printf(file_content);
+
+    fclose(flower_file);
+
+
+}
