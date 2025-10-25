1220a16676ee2766b7c5aec98bc44b6d1e585f9f
Wren L. Alexander <wren.alexander@student.manchester.ac.uk>
2025-10-25 14:23:07 +0100

    add new user script

--- general_setups/new_user.sh
+++ general_setups/new_user.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "todo"
+# Usage - ./<script>.sh <username> <password>
+
+useradd -m -s /bin/bash -f 0 $1
+echo "$1:$2" | chpasswd
+
+mkdir -p /hacknotts/secrets
+echo $2 > /hacknotts/secrets/$1_password
