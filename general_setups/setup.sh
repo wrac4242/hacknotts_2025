392626c1d9b82dda8cd229d5c6891b6bed73002e
Wren L. Alexander <wren.alexander@student.manchester.ac.uk>
2025-10-26 06:32:24 +0000

    test code

--- general_setups/setup.sh
+++ general_setups/setup.sh
@@ -29,13 +29,13 @@ to_run="mkdir /tmp/listening_to_you
 chown background_scripts:background_scripts /tmp/listening_to_you
 chmod 777 /tmp/listening_to_you"
 
-to_run > /root/startup.sh
+$to_run > /root/startup.sh
 chmod +x /root/startup.sh
 
 (crontab -l; echo "@reboot /root/startup.sh") | sort -u | crontab -
-./root/startup.sh # so its ran now
+/root/startup.sh # so its ran now
 
 (crontab -l -u background_scripts; echo "@reboot /home/background_scripts/background_scripts.sh") | sort -u | crontab -u background_scripts -
 
-su background_scripts -s /home/background_scripts/prerunning.sh 
-su background_scripts -s /home/background_scripts/background_scripts.sh
+runuser -u background_scripts '/home/background_scripts/prerunning.sh'
+runuser -u background_scripts '/home/background_scripts/background_scripts.sh'
